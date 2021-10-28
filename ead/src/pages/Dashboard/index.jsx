c8427a4b249622c995b02c0926cc65762e46f310
MarioBatalha <mariobatalha9@gmail.com>
2021-10-28 18:43:01 +0100

    update Dashboard

--- ead/src/pages/Dashboard/index.jsx
+++ ead/src/pages/Dashboard/index.jsx
@@ -1,68 +1,62 @@
-import { Layout, Menu, Breadcrumb } from 'antd';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
-import './style.scss';
+import { Layout, Menu, Image } from "antd";
+import {
+  AppstoreFilled,
+  SettingFilled,
+  MessageFilled,                                    
+} from "@ant-design/icons";
 
-const { SubMenu } = Menu;
-const { Header, Content, Sider } = Layout;
+import "./style.scss";
+const { Header, Content, Footer, Sider } = Layout;
 
 export const Dashboard = () => {
+  let fullYear = new Date().getFullYear();
   return (
-    <Layout>
-    <Header className="header">
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
-      </Menu>
-    </Header>
-    <Layout>
-      <Sider width={200} className="site-layout-background">
-        <Menu
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          style={{ height: '100%', borderRight: 0 }}
+    <>
+      <Layout className="content">
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          className="sider"
         >
-          <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-            <Menu.Item key="1">option1</Menu.Item>
-            <Menu.Item key="2">option2</Menu.Item>
-            <Menu.Item key="3">option3</Menu.Item>
-            <Menu.Item key="4">option4</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-            <Menu.Item key="5">option5</Menu.Item>
-            <Menu.Item key="6">option6</Menu.Item>
-            <Menu.Item key="7">option7</Menu.Item>
-            <Menu.Item key="8">option8</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-            <Menu.Item key="9">option9</Menu.Item>
-            <Menu.Item key="10">option10</Menu.Item>
-            <Menu.Item key="11">option11</Menu.Item>
-            <Menu.Item key="12">option12</Menu.Item>
-          </SubMenu>
-        </Menu>
-      </Sider>
-      <Layout style={{ padding: '0 24px 24px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
-        <Content
-          className="site-layout-background"
-          style={{
-            padding: 24,
-            margin: 0,
-            minHeight: 280,
-          }}
-        >
-          Content
-        </Content>
+          <Content  className="sidebar">
+            <Menu theme="light" mode="inline" defaultSelectedKeys={["4"]} style={{ color: "#B3BAB8"}}>
+            <Image height={100} width={200} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP02aG9ZVvHmqgT5ziWEMPRwuMhfQ41j-G7B6beeRi0I0nE80TVt26Dj4wyxRuaOX6iRE&usqp=CAU" alt="EAD plataforma" />
+              <Menu.Item key="1" icon={<AppstoreFilled  style={{ color: "#20BC9B", fontSize: 30}} />}>
+                Painel 
+              </Menu.Item>
+              <Menu.Item key="2" icon={<SettingFilled style={{ color: "#20BC9B", fontSize: 30}} />}>
+                Configurações
+              </Menu.Item>
+            </Menu>
+            <div>
+              <Content className="sidebar-bottom">
+                <Footer style={{ background: "#20BC9B", color: "#FFF", fontSize: 13, fontWeight: "bold", justifyContent: "center", alignItems: "center" }}>
+                  <MessageFilled style={{ color: "#FFF", fontSize: 30, marginRight: 5}} />
+                  Suporte
+                </Footer>
+              </Content>
+            </div>
+          </Content>
+        </Sider>
+        <Layout>
+          <Header
+            className="site-layout-sub-header-background"
+            style={{ padding: 0, background: "#FFF"}}>
+              <Content>Menu</Content>
+            </Header>
+          <Content className="container">
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 360 }}
+            >
+              content
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Created by © Mário Batalha - {fullYear}
+          </Footer>
+        </Layout>
       </Layout>
-    </Layout>
-  </Layout>,
-  mountNode
+    </>
   );
 };
