071fa1b834626368d5fbfe10ffc270eaf69de55b
MarioBatalha <mariobatalha9@gmail.com>
2021-10-28 14:09:06 +0100

    add Dashboard pages

--- ead/src/App.js
+++ ead/src/App.js
@@ -1,9 +1,10 @@
 import './App.scss';
+import { Dashboard } from './pages/Dashboard';
 
 export const App = () => {
   return (
-    <div className="App">
-    
-    </div>
+    <>
+      <Dashboard />
+    </>
   );
 }
